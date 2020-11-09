e592094f5f98ea999835c6e4fb65ece47103af0f
stepin-104624 <69069042+stepin-104624@users.noreply.github.com>
2020-11-09 11:33:27 +0530

    Update abcAnalysis.c

--- 3_Implementation/src/abcAnalysis.c
+++ 3_Implementation/src/abcAnalysis.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-void abc_analysis(int value,int number,int points){
-	if (value >= 6000)
-		printf ("The item is under A\n");
-	else if (value < 6000 && value >= 3000)
-		printf ("The item is under B\n");
-	else
-		printf ("The value is under C\n");
-	if (number >= 100)
-		printf ("vital\n");
-	else if (number < 100 && number >= 20)
-		printf ("Essential\n");
-	else
-		printf ("desirable\n");
-	if(points<=10&&points>=6)
-		printf("high success rate\n");
-	else
-		printf("low success rate change the plan\n");
+#include "abcanalysis.h"
+int main ()
+{
+  int val, num , point;
+  printf ("enter the value");
+  scanf ("%d", &val);
+  printf ("enter the number");
+  scanf ("%d", &num);
+  printf("enter the points");
+  scanf("%d",&point);
+  abc_analysis(val,num,point);
+  return 0;
 }
+
+
+
