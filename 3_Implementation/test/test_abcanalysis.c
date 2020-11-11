a63bed6ed2602c447ef24f06c1c39ff52a7fd38d
stepin-104624 <69069042+stepin-104624@users.noreply.github.com>
2020-11-11 15:02:37 +0530

    Update test_abcanalysis.c

--- 3_Implementation/test/test_abcanalysis.c
+++ 3_Implementation/test/test_abcanalysis.c
@@ -37,9 +37,9 @@ int main() {
 }
 /* Write all the test functions */
 void test_do_abcanalysis {
-  CU_ASSERT_EQUAL("The item is under A\n",abcanalysis(7000));
-   CU_ASSERT_EQUAL("The item is under vital\n",abcanalysis(700));
-   CU_ASSERT_EQUAL("high success rate\n",abcanalysis(7));
+  CU_ASSERT_EQUAL("The item is under A\n",abcanalysis("7000"));
+   CU_ASSERT_EQUAL("The item is under vital\n",abcanalysis("700"));
+   CU_ASSERT_EQUAL("high success rate\n",abcanalysis("7"));
 
   /* Dummy fail*/
 }
